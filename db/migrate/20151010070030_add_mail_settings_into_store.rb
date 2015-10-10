1549171e439e1272c7327e5f6b0589e9082f1c22
david <areq22@aliyun.com>
2015-10-10 22:31:48 +0800

    improving

--- db/migrate/20151010070030_add_mail_settings_into_store.rb
+++ db/migrate/20151010070030_add_mail_settings_into_store.rb
@@ -8,6 +8,7 @@ class AddMailSettingsIntoStore < ActiveRecord::Migration
     # :mail_auth_type, :string, default: Core::MailSettings::MAIL_AUTH[0]
     # :smtp_username, :string
     # :smtp_password, :string
+    add_column :spree_stores, :enable_mail_delivery, :boolean, default: false
     add_column :spree_stores, :mail_host, :string                # 'smtp.gmail.com'
     add_column :spree_stores, :mail_port, :integer, default: 25
     add_column :spree_stores, :secure_connection_type, :string, default: 'None'
