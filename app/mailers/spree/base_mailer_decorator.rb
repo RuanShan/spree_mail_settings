b5269d80b77a9567aa3c39dfeb0ffd0473423a38
qinghe <areq22@aliyun.com>
2015-12-10 20:22:37 -0800

    use mail_bcc instead mail_bcc_address

--- app/mailers/spree/base_mailer_decorator.rb
+++ app/mailers/spree/base_mailer_decorator.rb
@@ -1,11 +1,5 @@
   Spree::BaseMailer.class_eval do
 
-    #before_action :initialize_mail_settings #it is supported by rails4
-
-    def from_address
-      Spree::Store.current.mail_from_address
-    end
-
     def mail(headers={}, &block)
       super if Spree::Store.current.enable_mail_delivery
     end
